effdd5babd4d1b7c2fa2291f8c290de92e1b4e20
Buty <buty@Butys-Macbook-Pro.local>
2018-05-25 13:29:08 +0300

    email added

--- globalstart/ViewController.swift
+++ globalstart/ViewController.swift
@@ -8,8 +8,9 @@
 
 import UIKit
 import FirebaseAuth
+import MessageUI
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, MFMailComposeViewControllerDelegate {
     let emailField = "globalstart@cccsa.com"
     let pwdField = "winbuildsend"
     
@@ -65,31 +66,63 @@ class ViewController: UIViewController {
                         })
                     }
                 })
-//                let buttonThree = UIAlertAction(title: "Email Us", style: .default, handler: { (action) -> Void in
-//
+                let buttonThree = UIAlertAction(title: "Email Us", style: .default, handler: { (action) -> Void in
+
+                    
+                    
+                    let mailComposeViewController = self.configureMailController()
+                    if MFMailComposeViewController.canSendMail() {
+                        self.present(mailComposeViewController, animated: true, completion: nil)
+                    } else {
+                        self.showMailError()
+                    }
+                    
+                    
 //                            let vc = self.storyboard?.instantiateViewController(withIdentifier: "EmailUsViewController") as! EmailUsViewController
 //                           //  vc.recCatIdArray = categoryIdArray[indexPath.row]
 //                            //   self.navigationController?.pushViewController(vc, animated: true)
 //                            self.present(vc, animated: true, completion: nil)
-////                    let url = URL(string: "http://www.globalstart.cc")!
-////                    if UIApplication.shared.canOpenURL(url) {
-////                        UIApplication.shared.open(url, options: [:], completionHandler: nil)
-////                        //If you want handle the completion block than
-////                        UIApplication.shared.open(url, options: [:], completionHandler: { (success) in
-////                            print("Open url : \(success)")
-////                        })
-////                    }
-//                })
+//                    let url = URL(string: "http://www.globalstart.cc")!
+//                    if UIApplication.shared.canOpenURL(url) {
+//                        UIApplication.shared.open(url, options: [:], completionHandler: nil)
+//                        //If you want handle the completion block than
+//                        UIApplication.shared.open(url, options: [:], completionHandler: { (success) in
+//                            print("Open url : \(success)")
+//                        })
+//                    }
+                })
         
                 let buttonCancel = UIAlertAction(title: "OK", style: .cancel) { (action) -> Void in
                     print("Cancel Button Pressed")
                 }
         
                 alertController.addAction(buttonOne)
-               // alertController.addAction(buttonThree)
+                alertController.addAction(buttonThree)
                 alertController.addAction(buttonCancel)
         
                 present(alertController, animated: true, completion: nil)
+    }
+    
+    func configureMailController() -> MFMailComposeViewController {
+        let mailComposerVC = MFMailComposeViewController()
+        mailComposerVC.mailComposeDelegate = self
+        
+        mailComposerVC.setToRecipients(["mobilizeteens@gmail.com"])
+//        mailComposerVC.setSubject("  ")
+//        mailComposerVC.setMessageBody("Share Your story with us.", isHTML: false)
+        
+        return mailComposerVC
+    }
+    
+    func showMailError() {
+        let sendMailErrorAlert = UIAlertController(title: "Could not send email", message: "Your device could not send email", preferredStyle: .alert)
+        let dismiss = UIAlertAction(title: "Ok", style: .default, handler: nil)
+        sendMailErrorAlert.addAction(dismiss)
+        self.present(sendMailErrorAlert, animated: true, completion: nil)
+    }
+    
+    func mailComposeController(_ controller: MFMailComposeViewController, didFinishWith result: MFMailComposeResult, error: Error?) {
+        controller.dismiss(animated: true, completion: nil)
     }
 
     @IBAction func aboutUs(_ sender: Any) {
